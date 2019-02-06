0f7d909b5b463488b520bd13273f071754479f4e
Aaron Kollasch <awkollasch@gmail.com>
2019-02-06 11:11:13 -0500

    Add tests

---
+++ test/test_functions.py
@@ -0,0 +1,25 @@
+import unittest
+
+import torch
+import torch.nn.functional as F
+
+from functions import log_one_plus_exp, Normalize
+
+
+class TestFunctions(unittest.TestCase):
+    def test_log_one_plus_exp_range(self):
+        x = torch.FloatTensor([-1000, -100, -10., -1., -0.5, -0.1, 0.0, 0.1, 0.5, 1., 10, 100, 1000])
+        self.assertTrue(torch.allclose(
+            -F.logsigmoid(-x),
+            log_one_plus_exp(x)
+        ))
+
+    def test_log_one_plus_exp_randn(self):
+        x = torch.randn(1000)
+        self.assertTrue(torch.allclose(
+            -F.logsigmoid(-x),
+            log_one_plus_exp(x)
+        ))
+
+    def test_normalize(self):
+        self.assertTrue(Normalize.test())
